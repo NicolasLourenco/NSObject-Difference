d05c7bc1171bb63896c5186fb7dc0206baf31204
Nicolas Lourenco <nicolas.lourenco@keyrus.com>
2016-01-20 17:22:40 +0100

    Initial Commit

---
+++ Difference/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Difference
+//
+//  Created by Nicolas Lourenco on 20/01/16.
+//  Copyright © 2016 Nicolas Lourenco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Difference/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Difference
+//
+//  Created by Nicolas Lourenco on 20/01/16.
+//  Copyright © 2016 Nicolas Lourenco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
